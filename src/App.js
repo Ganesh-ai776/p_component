94971204918cb0562918a376ec753e6fdd16ac1a
Ganesh Maity <gm5094214@gmail.com>
2024-09-28 02:19:43 +0530

    components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className="con">
+      {/* <h1>
+        hello world
+      </h1> */}
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
